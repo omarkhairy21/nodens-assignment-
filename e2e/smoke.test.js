35b821a7dc3c2cdfd73e2fa4cb517127d893bc7e
omar khairy <omarkhairy21@gmail.com>
2020-10-28 11:10:27 +0200

    add login test suit for smoke.test

--- e2e/smoke.test.js
+++ e2e/smoke.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const User = require('../models/User');
 let page, browser;
 
-beforeAll(async () => {
+beforeEach(async () => {
   browser = await puppeteer.launch({
     headless: false,
   });
@@ -10,7 +10,7 @@ beforeAll(async () => {
   await page.goto('http://localhost:3002');
 });
 
-afterAll(async () => {
+afterEach(async () => {
   await browser.close();
 });
 
@@ -25,7 +25,7 @@ describe('Landing page loads', () => {
 
 describe('Signup', () => {
   it('Signup with valid credentials', async () => {
-    // fill the all form input
+    // fill the all form inputs
     await page.click('.signUp_btn');
     await page.type('.name_input', 'test66', { delay: 100 });
     await page.type('.email_input', 'test3565@test.com', { delay: 100 });
@@ -33,6 +33,22 @@ describe('Signup', () => {
     await page.type('.confirm_pass_input', 'test6635', { delay: 100 });
     // submit the form
     await page.click('.submit_signup');
+    await page.waitForNavigation(); // wait until navigate to /user/profiles
+    await page.waitForSelector('.header__h1');
+    // find the user name
+    const text = await page.$eval('.header__h1', (el) => el.innerHTML);
+    // assert the name of the user render correctly in his profile page
+    expect(text).toEqual('Hello, test66');
+  });
+});
+describe('login', () => {
+  it('login with valid credentials', async () => {
+    // fill the all form inputs
+    await page.click('.login_btn');
+    await page.type('.email_input', 'test3565@test.com', { delay: 100 });
+    await page.type('.pass_input', 'test6635', { delay: 100 });
+    // submit the form
+    await page.click('.submit_login');
     await page.waitForNavigation(); // wait until navigate to /user/profiles
     await page.waitForSelector('.header__h1');
     // find the user name
